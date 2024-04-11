e462aaf9d4f31c325691888a0ca9a25a4cb0834b
seohyun <dansunmusik7@gmail.com>
2024-04-11 18:01:45 +0900

    240411

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,28 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+
+    public static void main(String[] args){
+        //리터럴이란 값 자체를 의미한다.
+        //숫자형 리터럴 시스템이라는 패키지 안에서 아웃 .은 참조연산자 예를들어 하이미디어.별관.202호
+        System.out.println(123);
+
+        //실수형 리터럴
+        System.out.println(1.23);
+
+        //문자형 리터럴
+        System.out.println('A');
+
+        //문자열형 리터럴
+        System.out.println("hello world");
+
+        System.out.println("12345");
+        System.out.println("123.45");
+
+        //논리형 리터럴
+        System.out.println(true);
+        System.out.println(false);
+
+    }
+
+}
