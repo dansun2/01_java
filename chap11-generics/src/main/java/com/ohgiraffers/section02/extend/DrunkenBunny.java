f343362ea4fb91667a45db6d4af4a25dfb13aff5
dansun2 <dansunmusik7@gmail.com>
2024-04-29 15:51:20 +0900

    240429

---
+++ chap11-generics/src/main/java/com/ohgiraffers/section02/extend/DrunkenBunny.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section02.extend;
+
+public class DrunkenBunny extends Bunny{
+
+    @Override
+    public void cry() {
+        System.out.println("바ㄴ뉘바뉘 봐뉘봐뉘 당근 당근!@!#@$@#@$");
+    }
+}
