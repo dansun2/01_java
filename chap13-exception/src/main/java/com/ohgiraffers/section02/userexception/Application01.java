d6c70cf0d719eabad836ef6a44b16e2df9448076
dansun2 <dansunmusik7@gmail.com>
2024-05-02 10:55:38 +0900

    240502

--- chap13-exception/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
+++ chap13-exception/src/main/java/com/ohgiraffers/section02/userexception/Application01.java
@@ -25,28 +25,49 @@ public class Application01 {
 //            System.out.println("상품 가격을 정수로 다시 입력해주세요.");
 //        }
 
-        try{
-            et.checkEnoughMoney(-100,200);
-        }catch(PriceNegativeException e){
-            System.out.println("재품 가격이 음수야");
-        }catch(MoneyNegativeException e){
-            System.out.println("가진 돈이 음수야");
-        }catch(NotEnoughMoneyException e){
-            System.out.println("제품이 가진 돈보다 비싸");
-        }catch(Exception e){
-            System.out.println("뭐든 에러");
-        }
+//        try{
+//            et.checkEnoughMoney(-100,200);
+//        }catch(PriceNegativeException e){
+//            System.out.println("재품 가격이 음수야");
+//        }catch(MoneyNegativeException e){
+//            System.out.println("가진 돈이 음수야");
+//        }catch(NotEnoughMoneyException e){
+//            System.out.println("제품이 가진 돈보다 비싸");
+//        }catch(Exception e){
+//            System.out.println("뭐든 에러");
+//        }
+
+//        try {
+//
+//            et.checkEnoughMoney(100,200);
+//            et.checkEnoughMoney(200,-200);
+//
+//            // try 구문은 에러가 발생이 되면 동작을 중단한다.
+//            et.checkEnoughMoney(10,200);
+//        } catch(Exception e){
+//            //Exception은 모든 예외의 최상위 타입으로 아래의 예외를 검사하지 않음
+//        } catch (PriceNegativeException e) {
+//            System.out.println("제품의 가격이 음수일 수 없습니다.");
+//        } catch (MoneyNegativeException e) {
+//            System.out.println("가진 돈이 음수입니다.");
+//        } catch (NotEnoughMoneyException e) {
+//            System.out.println("제품의 가격이 가진돈보다 비싸네요.");
+//        }
 
         try {
-            et.checkEnoughMoney(-100,200);
-        } catch (PriceNegativeException e) {
-            throw new RuntimeException(e);
-        } catch (MoneyNegativeException e) {
-            throw new RuntimeException(e);
-        } catch (NotEnoughMoneyException e) {
-            throw new RuntimeException(e);
-        }
 
+            et.checkEnoughMoney(100,200);
+            et.checkEnoughMoney(200,-200);
+
+            // try 구문은 에러가 발생이 되면 동작을 중단한다.
+            et.checkEnoughMoney(10,200);
+        } catch (PriceNegativeException e) {
+            System.out.println("제품의 가격이 음수일 수 없습니다.");
+        } catch (MoneyNegativeException e) {
+            System.out.println("가진 돈이 음수입니다.");
+        } catch (NotEnoughMoneyException e) {
+            System.out.println("제품의 가격이 가진돈보다 비싸네요.");
+        }
         System.out.println("쇼핑종료");
     }
 }
